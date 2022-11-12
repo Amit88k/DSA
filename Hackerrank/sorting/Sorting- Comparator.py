fff9e66d65f93f2436e0531ece6911d52be1350f
Amit Khandelwal <amit.m.khandelwal@kipi.bi>
2022-11-12 17:24:07 +0530

    added Sorting- comparator.py

---
+++ Hackerrank/sorting/Sorting- Comparator.py
@@ -0,0 +1,23 @@
+from functools import cmp_to_key
+
+
+class Player:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def __repr__(self):
+        return repr((self.name, self.score))
+
+    def comparator(a, b):
+        if a.score > b.score:
+            return -1
+        elif a.score < b.score:
+            return 1
+        elif a.name > b.name:
+            return 1
+        else:
+            return -1
+
+
+n = int(input())
