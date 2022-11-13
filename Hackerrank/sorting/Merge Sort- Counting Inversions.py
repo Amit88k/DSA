05a7d021ee3193e108890f2d2349b7e1c609e976
Amit Khandelwal <amit.m.khandelwal@kipi.bi>
2022-11-13 17:19:31 +0530

    updated code with method 2

--- Hackerrank/sorting/Merge Sort- Counting Inversions.py
+++ Hackerrank/sorting/Merge Sort- Counting Inversions.py
@@ -70,6 +70,60 @@ def countInversions(arr):
     result = merging.mergeSort(arr, 0, len(arr) - 1)
     return result
 
+#Method 2:
+# def merge(arr, l, m, r):
+#         n1 = m - l + 1
+#         n2 = r - m
+#         L = [0] * (n1)
+#         R = [0] * (n2)
+#
+#         for i in range(n1):
+#             L[i] = arr[l + i]
+#
+#         for j in range(n2):
+#             R[j] = arr[m + 1 + j]
+#
+#         i = j = 0
+#         k = l
+#         count = 0
+#         while (i < n1 and j < n2):
+#             if L[i] <= R[j]:
+#                 arr[k] = L[i]
+#                 i += 1
+#             else:
+#                 arr[k] = R[j]
+#                 count += n1 - i
+#                 j += 1
+#
+#             k += 1
+#         while i < n1:
+#             arr[k] = L[i]
+#             k += 1
+#             i += 1
+#
+#         while j < n2:
+#             arr[k] = R[j]
+#             k += 1
+#             j += 1
+#         # print(count)
+#         return count
+#
+#
+# def mergeSort(arr, l, r):
+#     count = 0
+#     if l < r:
+#         mid = l + (r - l) // 2
+#         count += mergeSort(arr, l, mid)
+#         count += mergeSort(arr, mid + 1, r)
+#         count += merge(arr, l, mid, r)
+#         # count += c
+#     return count
+#
+#
+# def countInversions(arr):
+#     result = mergeSort(arr, 0, len(arr) - 1)
+#     return result
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
